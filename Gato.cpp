0bf081fe354487726b184ca4bc724c7e0e8b6d35
Luis Modesto <75289823+RepellantHail@users.noreply.github.com>
2022-11-18 09:50:12 -0600

    Update Gato.cpp

--- Gato.cpp
+++ Gato.cpp
@@ -11,12 +11,12 @@ class Node
 public:
 	Node();
 	~Node();
-	void checarVacio(char _juegoGato[3][3]); 
-	Node* crearNodo(char arrGato[3][3], int i, int j); 
-	void setGato(char _juegoGato[3][3], size_t pos_i, size_t pos_j, char _equipo);
+	void checarVacio(char _juegoGato[3][3])//Funcion que checa espacios vacios en el arrglo Gato
+	Node* crearNodo(char arrGato[3][3], int i, int j);//Funcion para iniciar un nodo y su correspondiente juego de Gato 
+	void setGato(char _juegoGato[3][3], size_t pos_i, size_t pos_j, char _equipo);//Funcion que copia los valores de gato de un nodo a otro
 private:
-	char juegoGato[3][3] = { ' ' };
-	std::vector<Node*> poiterV;
+	char juegoGato[3][3] = { ' ' };//Arrreglo de caracteres que guarda el juego de gato
+	std::vector<Node*> poiterV;//Arreglo dinamico para guardar los punteros
 };
 
 
@@ -26,34 +26,35 @@ Node::Node() {
 Node::~Node() {
 }
 
-void Node::setGato(char _juegoGato[3][3], size_t pos_i, size_t pos_j, char _equipo) {
+void Node::setGato(char _juegoGato[3][3], size_t pos_i, size_t pos_j, char _equipo) {//Recibe el arreglo gato, la posicion que debe cambiar y el caracter a ingresar
 	for (size_t i = 0; i < 3; i++)
 	{
 		for (size_t j = 0; j < 3; j++) {
 			juegoGato[i][j] = _juegoGato[i][j];
 			if (i == pos_i && j == pos_j)//Valor nulo que se debe modificar
-				juegoGato[i][j] = _equipo;
+				juegoGato[i][j] = _equipo;//El caracter puede ser X u O
 		}
 	}
 }
 
-Node* Node::crearNodo(char arrGato[3][3], int i, int j) {
-	Node* aux = new Node;
+Node* Node::crearNodo(char arrGato[3][3], int i, int j) {//Recibe el juego a inicializar y las posiciones a modificar
+	Node* aux = new Node;//Se inicializa un nodo
 	aux->setGato(arrGato, i, j, '#');//Ahora el nuevo nodo tiene un juego de gato inicializado
-	return aux;
+	//Se llena con un '#' solo para comprobar que se esta inicializando
+	return aux;//Por ultimo regresa
 }
 
 void Node::checarVacio(char _juegoGato[3][3]) {//Funcion recursiva para crear nodos, recibe caracter actual
 	for (size_t i = 0; i < 3; i++)
 	{
-		for (size_t j = 0; j < 3; j++) {
+		for (size_t j = 0; j < 3; j++) {//Se accesede al arreglo por medio de un for anidado
 			if (_juegoGato[i][j] == '$') {//Si nodo vacio
 				//Crear Nodo con posibles juegos, copiar datos del arreglo actual a uno nuevo
 				//Se crea un arreglo con el caracter en la posicion vacia
 				//Se envia la posicion actual en ejes de i y j 
 				this->poiterV.push_back(crearNodo(_juegoGato, i, j));
 				//Se inserta el Caracter actual en las distintas posiciones
-				_juegoGato[i][j] = '#';
+				_juegoGato[i][j] = '#';//Se asigan '#' para las posiciones que ya se revisaron
 			}
 			else {
 			}
